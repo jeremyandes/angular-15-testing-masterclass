3ad4ec0eebc3e079bc45dacf48d231b6ef5da516
jeremyandes <jeremyandes25@gmail.com>
2023-02-14 11:02:26 -0300

    7. Unit Testing with Jasmine - Introduction to Jasmine Spies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,11 +3,15 @@ import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
     it('should add two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
+        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+        const calculator = new CalculatorService(logger);
 
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
+
+        expect(logger.log).toHaveBeenCalledTimes(1);
     });
 
     it('should substract two numbers', () => {
