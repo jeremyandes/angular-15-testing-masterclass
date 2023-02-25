93e738ec1d220c1c4ca484ef10373262ebcfd9cf
jeremyandes <jeremyandes25@gmail.com>
2023-02-25 16:09:01 -0300

    20. Angular Component Testing - DOM Interaction

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,9 +1,14 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CoursesModule } from '../courses.module';
 import { CoursesCardListComponent } from './courses-card-list.component';
+import { COURSES } from '../../../../server/db-data';
+import { setupCourses } from '../common/setup-test-data';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 describe('CoursesCardListComponent', () => {
     let component: CoursesCardListComponent;
     let fixture: ComponentFixture<CoursesCardListComponent>;
+    let debugElement: DebugElement;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
@@ -13,15 +18,21 @@ describe('CoursesCardListComponent', () => {
             .then(() => {
                 fixture = TestBed.createComponent(CoursesCardListComponent);
                 component = fixture.componentInstance;
+                debugElement = fixture.debugElement;
             });
     }));
 
     it('should create the component', () => {
         expect(component).toBeTruthy();
     });
+
     it('should display the course list', () => {
-        pending();
+        component.courses = setupCourses();
+        const cards = debugElement.queryAll(By.css('.course-card'));
+        expect(cards).toBeTruthy('Could not find cards');
+        expect(cards.length).toBe(12, 'Unexpected number of courses');
     });
+
     it('should create the first course', () => {
         pending();
     });
