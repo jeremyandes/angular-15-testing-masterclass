78b75bac230b36555d9c873abb1c4630469a4bc3
jeremyandes <jeremyandes25@gmail.com>
2023-02-25 13:44:51 -0300

    16. Angular HTTP Testing - Checking HTTP Request Parameters

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -5,7 +5,7 @@ import {
     HttpTestingController,
     TestRequest,
 } from '@angular/common/http/testing';
-import { COURSES } from '../../../../server/db-data';
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 import { Course } from '../model/course';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -92,6 +92,28 @@ describe('CoursesService', () => {
         req.flush('Save course failed', {
             status: 500,
             statusText: 'Internal Server Error',
+        });
+    });
+
+    it('should find a list of lessons', () => {
+        coursesService.findLessons(12).subscribe({
+            next: (lessons) => {
+                expect(lessons).toBeTruthy();
+                expect(lessons.length).toBe(3);
+            },
+        });
+
+        const req = httpTestingController.expectOne((req) => req.url === '/api/lessons');
+
+        expect(req.request.method).toEqual('GET');
+        expect(req.request.params.get('courseId')).toEqual('12');
+        expect(req.request.params.get('filter')).toEqual('');
+        expect(req.request.params.get('sortOrder')).toEqual('asc');
+        expect(req.request.params.get('pageNumber')).toEqual('0');
+        expect(req.request.params.get('pageSize')).toEqual('3');
+
+        req.flush({
+            payload: findLessonsForCourse(12).slice(0, 3),
         });
     });
 
