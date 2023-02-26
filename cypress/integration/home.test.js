77047e7a3269d144827b9e18cd6097a513efd6f5
jeremyandes <jeremyandes25@gmail.com>
2023-02-25 22:19:09 -0300

    40. Simulating User Interaction in Cypress End to End Tests

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Home Page', () => {
-    it('should display a list of courses', () => {
+    beforeEach(() => {
         cy.fixture('courses.json').as('coursesJSON');
 
         cy.intercept('GET', '/api/courses', {
@@ -9,8 +9,18 @@ describe('Home Page', () => {
         });
 
         cy.visit('/');
+    });
+    it('should display a list of courses', () => {
         cy.contains('All Courses');
 
         cy.get('mat-card').should('have.length', 9);
+    });
+
+    it('should display the advanced courses', () => {
+        cy.get('.mat-mdc-tab').should('have.length', 2);
+        cy.get('.mat-mdc-tab').last().click();
+        cy.get('.mat-mdc-card-title').its('length').should('be.gt', 1);
+        cy.wait(500);
+        cy.get('.mat-mdc-card-title').first().should('contain.text', 'Angular Security Course');
     });
 });
