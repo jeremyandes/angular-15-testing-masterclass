5d1eb1e207cd72054bfe9d9aa6f2b5cac58c92d1
jeremyandes <jeremyandes25@gmail.com>
2023-02-25 21:58:43 -0300

    38. Angular E2E (End To End Test) Hello World with Cypress

---
+++ cypress/integration/home.test.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+describe('Home Page', () => {
+    it('should display a list of courses', () => {
+        cy.visit('/');
+
+        cy.contains('All Courses');
+    });
+});
