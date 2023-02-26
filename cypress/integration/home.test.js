162b021be4d2631bf69f1eb14fcdae4c954a121a
jeremyandes <jeremyandes25@gmail.com>
2023-02-25 22:08:55 -0300

    39. How to Mock an HTTP Backend using Cypress

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -2,8 +2,15 @@
 
 describe('Home Page', () => {
     it('should display a list of courses', () => {
-        cy.visit('/');
+        cy.fixture('courses.json').as('coursesJSON');
 
+        cy.intercept('GET', '/api/courses', {
+            fixture: 'courses',
+        });
+
+        cy.visit('/');
         cy.contains('All Courses');
+
+        cy.get('mat-card').should('have.length', 9);
     });
 });
